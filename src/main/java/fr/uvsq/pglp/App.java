fa88fe32be160d467471ff6a80c9d98885d81190
MehdiJames <44882144+MehdiJames@users.noreply.github.com>
2020-05-24 14:43:53 +0200

    first commit

---
+++ src/main/java/fr/uvsq/pglp/App.java
@@ -0,0 +1,19 @@
+package fr.uvsq.pglp;
+
+import fr.uvsq.pglp.builder.composite.iterator.AnnuairePersonnels;
+
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        AnnuairePersonnels.main(args);
+        
+    }
+}
